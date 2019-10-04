76c25adf77a98f2ebd1382415664f77f7e8edc98
Asif ahmed Sohan <asifsohanz@yahoo.com>
2019-10-04 17:31:59 +0600

    Bottom navigation view menu with library

--- app/src/main/java/com/asifahmedsohan/outstagram/MainActivity.java
+++ app/src/main/java/com/asifahmedsohan/outstagram/MainActivity.java
@@ -3,12 +3,32 @@ package com.asifahmedsohan.outstagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.asifahmedsohan.outstagram.Utils.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: starting");
+
+        setupBottomNavigationView();
+    }
+
+
+    /**
+     * BottomNavigationView setup
+     */
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting bottom navigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx)findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+
     }
 }
---
+++ app/src/main/java/com/asifahmedsohan/outstagram/Utils/BottomNavigationViewHelper.java
@@ -0,0 +1,21 @@
+package com.asifahmedsohan.outstagram.Utils;
+
+
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class BottomNavigationViewHelper {
+
+    private static final String TAG = "BottomNavigationViewHel";
+
+    public static void setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx){
+
+        Log.d(TAG, "setupBottomNavigationView: Setting up BottomNavigationView");
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);
+    }
+
+}
